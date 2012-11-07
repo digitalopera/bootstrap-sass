68be6a7353ea4f4f2dc91ce7732b59c3673662a9
Grant Klinsing <gklinsing@gmail.com>
2012-11-07 13:56:12 -0700

    allow use of functions for position

--- vendor/assets/javascripts/bootstrap-tooltip.js
+++ vendor/assets/javascripts/bootstrap-tooltip.js
@@ -105,57 +105,65 @@
         , tp
 
       if (this.hasContent() && this.enabled) {
-        $tip = this.tip()
-        this.setContent()
+        $tip = this.tip();
+        this.setContent();
 
         if (this.options.animation) {
-          $tip.addClass('fade')
+          $tip.addClass('fade');
         }
 
         placement = typeof this.options.placement == 'function' ?
           this.options.placement.call(this, $tip[0], this.$element[0]) :
-          this.options.placement
+          this.options.placement;
 
-        inside = /in/.test(placement)
+        if (typeof placement === 'object'){
+          tp = placement;
+          inside = false;
+        }
+        else {
+          inside = /in/.test(placement);
+        }
 
         $tip
           .remove()
           .css({ top: 0, left: 0, display: 'block' })
-          .appendTo(inside ? this.$element : document.body)
+          .appendTo(inside ? this.$element : document.body);
 
-        pos = this.getPosition(inside)
+        pos = this.getPosition(inside);
 
-        actualWidth = $tip[0].offsetWidth
-        actualHeight = $tip[0].offsetHeight
+        actualWidth = $tip[0].offsetWidth;
+        actualHeight = $tip[0].offsetHeight;
 
-        switch (inside ? placement.split(' ')[1] : placement) {
-          case 'bottom':
-            tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2}
-            break
-          case 'top':
-            tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2}
-            break
-          case 'left':
-            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth}
-            break
-          case 'right':
-            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width}
-            break
+        if(typeof placement === 'string'){
+          switch (inside ? placement.split(' ')[1] : placement) {
+            case 'bottom':
+              tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2};
+              break;
+            case 'top':
+              tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2};
+              break;
+            case 'left':
+              tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth};
+              break;
+            case 'right':
+              tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
+              break;
+          }
         }
 
         $tip
           .css(tp)
           .addClass(placement)
-          .addClass('in')
+          .addClass('in');
       }
     }
 
   , setContent: function () {
       var $tip = this.tip()
-        , title = this.getTitle()
+        , title = this.getTitle();
 
-      $tip.find('.tooltip-inner')[this.options.html ? 'html' : 'text'](title)
-      $tip.removeClass('fade in top bottom left right')
+      $tip.find('.tooltip-inner')[this.options.html ? 'html' : 'text'](title);
+      $tip.removeClass('fade in top bottom left right');
     }
 
   , hide: function () {
